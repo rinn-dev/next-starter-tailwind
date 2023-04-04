362f49a9b188c89f1625fdb49715792bbc3dba46
rinn-dev <mr.htetmyatlinn404@gmail.com>
2023-04-05 01:27:16 +0630

    chore: add husky, commit-lint, eslint and prettier with lintstaged

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Homepage() {
+  return (
+    <main>
+      <h1 className="mx-auto my-6 text-center text-4xl text-red-700">
+        Hello Tailwind
+      </h1>
+    </main>
+  );
+}
